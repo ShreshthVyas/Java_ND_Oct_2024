42847704f8bee943ebaba3a5b5b1ba3fb9d39557
ShreshthVyas <shreshth.vyas.2000@gmail.com>
2024-12-21 15:45:41 +0530

    Recursion

--- Java_13th_Oct_ND/src/Lecture17/SumOfNumbers.java
+++ Java_13th_Oct_ND/src/Lecture17/SumOfNumbers.java
@@ -1,11 +1,12 @@
 package Lecture17;
 
 public class SumOfNumbers {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int n = 10;
-		System.out.println(sumN(n));
+//		
+//		System.out.println(sumN(n));
+		System.out.println(sum2(1));
 		
 	}
 	public static int sumN(int n) {
@@ -14,6 +15,14 @@ public class SumOfNumbers {
 		}
 		int sum_n_minus_one = sumN(n-1);
 		return sum_n_minus_one + n;
+	}
+	
+	public static int sum2(int n) {
+		if(n>5) {
+			return 0;
+		}
+		
+		return sum2(n+1) + n ;
 	}
 
 }
