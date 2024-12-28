b9f9a74433728c05c87e1e5507046374dc7cb84b
ShreshthVyas <shreshth.vyas.2000@gmail.com>
2024-12-28 13:44:25 +0530

    Recursion-4

--- Java_13th_Oct_ND/src/Lecture20/KepadCombinationLeetcode.java
+++ Java_13th_Oct_ND/src/Lecture20/KepadCombinationLeetcode.java
@@ -1,10 +1,37 @@
 package Lecture20;
 
-public class KepadCombinationLeetcode {
+import java.util.ArrayList;
+import java.util.List;
 
+public class KepadCombinationLeetcode {
+	static String arr[] = { "*", "*", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+	static List<String> list = new ArrayList<>();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	}
 
+	public static List<String> letterCombinations(String digits) {
+		keypad(digits, "");
+		return list;
+	}
+	public static void keypad(String s, String ans) {
+		// TODO Auto-generated method stub
+		if(s.length()== 0) {
+			if(ans!=""){
+                list.add(ans);
+            }
+			return;
+		}
+		
+		char ch =  s.charAt(0);
+		int idx = ch - '0';
+		String temp= arr[idx];
+		
+		for (int i = 0; i < temp.length(); i++) {
+			keypad(s.substring(1), ans+ temp.charAt(i));
+		}
+		
 	}
 
 }
