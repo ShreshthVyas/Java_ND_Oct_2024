360e5510e09e1e4edd196bc2d90909c258e81c76
ShreshthVyas <shreshth.vyas.2000@gmail.com>
2025-03-03 11:10:51 +0530

    LL- 1

---
+++ Java_13th_Oct_ND/src/Lecture31/JavaLLDemo.java
@@ -0,0 +1,17 @@
+package Lecture31;
+import java.util.LinkedList;
+public class JavaLLDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<Integer> ll = new LinkedList<>();
+		ll.add(10);
+		ll.add(10);
+		ll.add(10);
+		ll.add(10);
+		ll.add(10);
+		ll.add(10);
+		System.out.println(ll);
+	}
+
+}
